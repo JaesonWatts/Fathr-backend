7bad37ee3485546dfb91ca4ba24399671217b475
jaesonwatts <jaeson.watts@gmail.com>
2020-11-18 19:52:54 -0700

    configure session store to work in all env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_fathr_app", domain: "fathr.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_fathr_app", domain: "fathr.com"  
+else
+  Rails.application.config.session_store :cookie_store, key: "_fathr_app"
+end
